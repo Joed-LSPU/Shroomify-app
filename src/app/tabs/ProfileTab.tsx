1fbf9b3b87790beea3a49860340066633d762450
Joed Michael Meraña <joedmichael.merana@lspu.edu.ph>
2025-08-13 18:33:47 +0800

    Initialize

---
+++ src/app/tabs/ProfileTab.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const ProfileTab = () => {
+  return (
+    <div className="p-6">
+      {/* Profile content goes here */}
+      <p className="text-white">User profile here.</p>
+    </div>
+  );
+};
+
+export default ProfileTab;
